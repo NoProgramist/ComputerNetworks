3f7902ccd29d48ff9f9869ee06e138647a9b16ac
asmeralt <asmeralt@gmail.com>
2017-12-02 12:14:12 +0200

    Initial commit

---
+++ src/main/java/ua/edu/lnu/cns_webserver/CnsWebserverApplication.java
@@ -0,0 +1,12 @@
+package ua.edu.lnu.cns_webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsWebserverApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/edu/lnu/cns_webserver/CnsWebserverApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.edu.lnu.cns_webserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CnsWebserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
