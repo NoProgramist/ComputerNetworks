93497ade076c20c775423ba2ff15ac4a62013e56
asmeralt <asmeralt@gmail.com>
2017-12-02 17:08:05 +0200

    Removed unused canvas. Svg element now is static html element

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,8 +3,7 @@ var width  = 960,
     height = 500,
     colors = d3.scale.category10();
 
-var svg = d3.select('body')
-  .append('svg')
+var svg = d3.select('#canvas')
   .attr('oncontextmenu', 'return false;')
   .attr('width', width)
   .attr('height', height);
